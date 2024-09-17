dc567288f5aabf01c842825bd1ab46900af3b953
Luna Del Valle Blazquez <ldel-val@c1r10s4.42madrid.com>
2024-09-17 16:35:58 +0200

    upload 1

---
+++ libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldel-val <ldel-val@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 14:44:21 by ldel-val          #+#    #+#             */
+/*   Updated: 2024/09/17 15:23:57 by ldel-val         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+size_t	ft_strlen(const char *s);
+
+void	*memset(void *s, int c, size_t n);
+void	bzero(void *s, size_t n);
+#endif
