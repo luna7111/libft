a4e4ff1665e36b567d7220557b7d75b0ab4ddb65
Luna del Valle <luna7111@tuta.io>
2024-12-07 19:13:19 +0100

    changed ft_split_file to make ir more efficient. Fixed some norme errors

--- io/input/ft_split_file.c
+++ io/input/ft_split_file.c
@@ -6,19 +6,43 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/12/04 11:09:45 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/12/04 11:20:35 by ldel-val          ``                     */
+/*   Updated: 2024/12/07 19:09:29 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
+char	**realloc_array(char **array, int size, int new_size)
+{
+	char	**new_array;
+
+	new_array = malloc(new_size * sizeof(char *));
+	if (!new_array)
+		return (NULL);
+	while (size)
+	{
+		new_array[size - 1] = array[size - 1];
+		size --;
+	}
+	free(array);
+	return (new_array);
+}
+
 char	**split_file(int fd)
 {
-	char *raw_file;
-	char **split_file;
+	char	**split_file;
+	int		line;
 
-	raw_file = get_whole_file(fd);
-	split_file = ft_split(raw_file, '\n');
-	free(raw_file);
+	split_file = malloc(sizeof(char *));
+	if (!split_file)
+		return (NULL);
+	line = 0;
+	split_file[0] = get_next_line(fd);
+	while (split_file[line])
+	{
+		line ++;
+		realloc_array(split_file, line, line + 1);
+		split_file[line] = get_next_line(fd);
+	}
 	return (split_file);
 }
